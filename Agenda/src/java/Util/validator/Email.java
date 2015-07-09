99cc61c87dd757dd899711d88ec4f7a3d3ed1791
Jairo Reyes Rosa <ing.jairoreyes@gmail.com>
2015-07-09 15:49:46 -0300

    corregido el problema del menu general y de la valcion del correo Video 11 Min 05:32

--- Agenda/src/java/Util/validator/Email.java
+++ Agenda/src/java/Util/validator/Email.java
@@ -32,7 +32,7 @@ public class Email implements Validator{
         else
             label = htmlInputText.getLabel();
         
-        Pattern pattern = Pattern.compile("^[_A-Za-z0-9-\\\\+]+(\\\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\\\.[A-Za-z0-9]+)*(\\\\.[A-Za-z]{2,})$;");
+        Pattern pattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
         Matcher matcher = pattern.matcher((CharSequence)value);
         
         if(!matcher.matches())
