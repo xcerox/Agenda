e2b3862d4c1a7648d5fb35bfd199e8cf0e44dc98
Jairo Reyes Rosa <ing.jairoreyes@gmail.com>
2015-07-09 09:32:44 -0300

    Monstrando mensaje con faceContext y validando la clave

--- Agenda/src/java/ManagedBean/Request/MbRUsuario.java
+++ Agenda/src/java/ManagedBean/Request/MbRUsuario.java
@@ -14,6 +14,8 @@ import Dao.daoTusuario;
 import org.hibernate.Session;
 import HibernateUtil.HibernateUtil;
 import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import org.hibernate.Transaction;
 
 /**
@@ -48,22 +50,42 @@ public class MbRUsuario {
         this.transaction = null;
     }
 
+    private void showError(String text) {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", text));
+    }
+    
+    private void showInformation(String text){
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Información", text));
+    }
+
+    private boolean checkUser() {
+        boolean result = true;
+        if (!this.tusuario.getContrasenia().equals(this.txtContraseniaRepita)) {
+            showError("Las contraseñas no coinciden");
+            result = false;
+        }
+        return result;
+    }
+
     public String register() throws RuntimeException {
+        if (checkUser()) {
+            try {
+                cleanConnection();
+                this.tusuario.setContrasenia(Encrypt.sha512(this.tusuario.getContrasenia()));
+                daoTusuario daoTusuario = new daoTusuario();
 
-        try {
-            cleanConnection();
-            this.tusuario.setContrasenia(Encrypt.sha512(this.tusuario.getContrasenia()));
-            daoTusuario daoTusuario = new daoTusuario();
+                this.session = HibernateUtil.getSessionFactory().openSession();
+                this.transaction = this.session.beginTransaction();
 
-            this.session = HibernateUtil.getSessionFactory().openSession();
-            this.transaction = this.session.beginTransaction();
-
-            daoTusuario.insert(this.tusuario, this.session);
-            this.transaction.commit();
-
+                daoTusuario.insert(this.tusuario, this.session);
+                this.transaction.commit();
+                showInformation("Usuario agregado.");                
+                return "/usuario/registrar";
+            } catch (Exception error) {
+                throw new RuntimeException("Error al registrar usuario", error);
+            }
+        } else {
             return "/usuario/registrar";
-        } catch (Exception error) {
-            return "#";
         }
     }
 
